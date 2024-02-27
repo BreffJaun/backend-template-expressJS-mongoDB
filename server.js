e58dfb96af91d56c35adbcf90dce8ff2d60d0818
BreffJaun <braun_jeff@web.de>
2024-02-27 10:20:21 +0100

    Replaced wrongPathRoutes & wrongController with invalidRoute

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 // I M P O R T:  E X T E R N A L  D E P E N D E N C I E S
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
-// I M P O R T:  E N V 
-import { PORT } from './config/config.js';
+// I M P O R T:  E N V
+import { PORT } from "./config/config.js";
 
-// I M P O R T:  C O M P O N E N T S 
-import { connectToDatabase } from './config/database.js';
+// I M P O R T:  C O M P O N E N T S
+import { connectToDatabase } from "./config/database.js";
 
 // I M P O R T:  M I D D L E W A R E  H A N D L E R
-import wrongRoutes from './routes/wrongPath.js';
-import errorHandler from './middleware/errorhandler.js';
+import invalidRoute from "./routes/invalidRoute.js";
+import errorHandler from "./middleware/errorhandler.js";
 
 // I M P O R T:  R O U T E S
-import usersRouter from './routes/users.js';
-
+import usersRouter from "./routes/users.js";
 
 // ==============================================================
 
@@ -26,27 +25,26 @@ const app = express();
 // M I D D L E W A R E
 
 // SERVER MIDDLEWARE
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
+app.use(
+  cors()
   // {
   //   origin: 'http://localhost:3000', // fill in here render address
   // if you want to add more adresses in cors, make an array with single strings.
   //   credentials: true
   // }
-  )
 );
 app.use(morgan("dev"));
 
 // ROUTER MIDDLEWARE
 // USERS
-app.use('/users', usersRouter);
-
+app.use("/users", usersRouter);
 
 // WRONG PATH HANDLER
-app.use('*', wrongRoutes);
+app.use("*", invalidRoute);
 
 // ERROR HANDLER
 app.use(errorHandler);
@@ -58,9 +56,5 @@ connectToDatabase();
 
 // S E R V E R - S T A R T
 app.listen(PORT, () => {
-  console.log('Server runs on Port: ' + PORT, '🔄');
+  console.log("Server runs on Port: " + PORT, "🔄");
 });
-
-
-
-

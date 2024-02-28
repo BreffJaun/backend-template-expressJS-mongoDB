a54ba5ca3b974239a2afbeb1cf78961e914a4707
BreffJaun <braun_jeff@web.de>
2024-02-28 11:48:34 +0100

    Updated validator.js => the error.array is send to errorHandler, to display the error more centralized there. Also customized because of that the errorHandler

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -3,23 +3,48 @@ import { ERROR_HANDLING_OPTIONS } from "../config/config.js";
 
 // E R R O R   H A N D L E R
 const errorHandler = (err, req, res, next) => {
+  let isValidationError = Array.isArray(err);
   // CONSOLE OUTPUT
-console.log("\n========= E R R O R =========");
-console.error(`Name: ${err.name}\nMessage: ${err.message}\nStatusCode: ${err.statusCode || 'N/A'}`);
-console.log("========= E R R O R =========\n");
-
+  console.log("\n========= E R R O R =========");
+  if (isValidationError) {
+    console.error(
+      `Name: Validation Error\nMessage: Validation failed\nErrors: ${JSON.stringify(
+        err,
+        null,
+        2
+      )}`
+    );
+  } else {
+    console.error(
+      `Name: ${err.name}\nMessage: ${err.message}\nStatusCode: ${
+        err.statusCode || "500"
+      }`
+    );
+  }
+  console.log("========= E R R O R =========\n");
 
   // RESPONSE OUTPUT
-  const statusCode = err.statusCode || 500; 
+  const statusCode = err.statusCode || 500;
   let errorResponse = {
     error: {
       status: statusCode,
-      message: err.message || 'Internal Server Error'
-    }
+      message: err.message || "Internal Server Error",
+    },
   };
 
+  // CUSTOMIZE ERROR (IN CASE OF VALIDATION ERROR)
+  if (isValidationError) {
+    statusCode = 422;
+    errorResponse.error.status = statusCode;
+    errorResponse.error.message = "Validation failed";
+    errorResponse.errors = err;
+  }
+
   // Check whether stack traces should be displayed
-  if (ERROR_HANDLING_OPTIONS.showStack && process.env.NODE_ENV === 'development') {
+  if (
+    ERROR_HANDLING_OPTIONS.showStack &&
+    process.env.NODE_ENV === "development"
+  ) {
     errorResponse.error.stack = err.stack;
   }
   res.status(statusCode).send(errorResponse);
